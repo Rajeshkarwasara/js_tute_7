73836dbb5eb04857324662a8c117210442b7c574
Rajesh karwasara <140477625+Rajeshkarwasara@users.noreply.github.com>
2023-08-20 21:11:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+let arry=["ram","raj","rajesh","rajkumar","princes"]
+console.log(arry)
+
+let tostring=arry.toString();
+console.log(tostring);
+
+// let pop=arry.pop();
+// console.log(arry.pop())
+arry.pop();
+
+// for(let g=0; g<2; g++){
+// arry.pop();
+// }
+
+
+
+let pushh=arry.push("raju")
+console.log(pushh)
+
+console.log(arry.join(">"))
+
+
+// for(let s=0; s<=100; s++){
+//     arry.shift();
+//     document.write("raju"," ")
+// }
+arry.shift();
+arry.shift();
+arry.unshift("ramesh");
+
+
+
+
+
+
+let are1=[1,2,3,4]
+let are2=[5,6,7,8,9,10]
+
+console.log(are1+are2)
+
+let con=are1.concat(are2)
+console.log(con)
+
+
+are2.splice(2,3,)
+console.log(are2)
+
+let are4=are1.slice(1,4)
+console.log(are4)
